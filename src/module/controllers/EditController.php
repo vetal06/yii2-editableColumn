61ea4e90afd893ed6e0065d2fc50bfb6f835f55e
VSK <bbssvv06@gmail.com>
2018-10-03 11:38:44 +0300

    add fix for scenario validation

--- src/module/controllers/EditController.php
+++ src/module/controllers/EditController.php
@@ -3,6 +3,8 @@ namespace vsk\editableColumn\module\controllers;
 
 use vsk\editableColumn\EditableColumn;
 use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+use yii\web\BadRequestHttpException;
 use \yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\web\Response;
@@ -24,6 +26,7 @@ class EditController extends Controller
 
        $modelClassEncode = \Yii::$app->request->post('class');
        $attribute = \Yii::$app->request->post('attribute');
+       \Yii::$app->response->format = Response::FORMAT_JSON;
        if (empty($modelClassEncode)) {
            throw new \yii\web\BadRequestHttpException('class is empty!');
        }
@@ -37,12 +40,32 @@ class EditController extends Controller
            throw new NotFoundHttpException('Model Not found!');
        }
        $modal->setScenario(EditableColumn::MODEL_SCENARIO_EDITABLE_COLUMN);
+       $scenarioAttributes = $this->getScenarionAttributes($modal);
+       if (!in_array($attribute,$scenarioAttributes)) {
+           throw new BadRequestHttpException('This attribute not set in you model rules scenario');
+       }
        $modal->load(\Yii::$app->request->post());
-       \Yii::$app->response->format = Response::FORMAT_JSON;
        if ($modal->save(true, [$attribute])) {
            return ['output'=>$modal->getAttribute($attribute), 'message'=>''];
        } else {
            return ['output'=>'', 'message'=> $modal->getFirstError($attribute)];
        }
+    }
+
+    /**
+     * @param ActiveRecord $model
+     * @return mixed
+     */
+    protected function getScenarionAttributes(ActiveRecord $model)
+    {
+        $scenarios = [];
+        foreach ($model->getValidators() as $validator) {
+            foreach ($validator->on as $name) {
+                foreach ($validator->attributes as $attribute) {
+                    $scenarios[$name] = [$attribute];
+                }
+            }
+        }
+        return ArrayHelper::getValue($scenarios, EditableColumn::MODEL_SCENARIO_EDITABLE_COLUMN);
     }
 }
